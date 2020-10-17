822f60b50014c53246c3af10b4d80b6c816478ba
Nathan Orlowski <norlowski@gmail.com>
2020-10-17 09:00:08 -0400

    Updates

---
+++ precache-manifest.6001c696daf93aa07fefa631c7e223dc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89b132a1a1f7440cfb5efa88c7496f6",
+    "url": "/ProjectTigerLilly/index.html"
+  },
+  {
+    "revision": "a34a75401434b6a5d1db",
+    "url": "/ProjectTigerLilly/static/css/2.da70e027.chunk.css"
+  },
+  {
+    "revision": "3d8732f0c0cdc9c7484d",
+    "url": "/ProjectTigerLilly/static/css/main.2dce71ba.chunk.css"
+  },
+  {
+    "revision": "a34a75401434b6a5d1db",
+    "url": "/ProjectTigerLilly/static/js/2.1342799a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ProjectTigerLilly/static/js/2.1342799a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d8732f0c0cdc9c7484d",
+    "url": "/ProjectTigerLilly/static/js/main.398467aa.chunk.js"
+  },
+  {
+    "revision": "4d89680d7bad25f46d3d",
+    "url": "/ProjectTigerLilly/static/js/runtime-main.fa411bc2.js"
+  },
+  {
+    "revision": "7255ba8e48be7967b2eab6482f80d641",
+    "url": "/ProjectTigerLilly/static/media/Agadmator.7255ba8e.png"
+  },
+  {
+    "revision": "5ed18b9569b9f25934888494fd3ac7ad",
+    "url": "/ProjectTigerLilly/static/media/Alexandra-Botez.5ed18b95.png"
+  },
+  {
+    "revision": "672b4453b3ba55870a891d10435455b2",
+    "url": "/ProjectTigerLilly/static/media/ChessNetwork.672b4453.png"
+  },
+  {
+    "revision": "6db6e02b05ef25d2cf4708a39d37558f",
+    "url": "/ProjectTigerLilly/static/media/Christof-Sielecki.6db6e02b.png"
+  },
+  {
+    "revision": "23abe5cfb2dd1dae3e17afc72d00a758",
+    "url": "/ProjectTigerLilly/static/media/Eric-Rosen.23abe5cf.png"
+  },
+  {
+    "revision": "64d48b24048c9011c9b2407b704303e1",
+    "url": "/ProjectTigerLilly/static/media/John-Bartholomew.64d48b24.png"
+  }
+]);
